ca63853faf5c6c6904d8ddbe21ad050ec878e98e
Nkurumwa <nkurumwa51@gmail.com>
2019-12-02 15:18:57 +0300

    adds models

--- hood/models.py
+++ hood/models.py
@@ -1,141 +1,127 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
-from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Neighbourhood(models.Model):
-    locality = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
-    occupants_count = models.IntegerField(blank=True)
-    user_profile = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hoods', blank=True, default=1)
-    date = models.DateTimeField(default=timezone.now)
+# Create your models here.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-    def __str__(self):
-        return self.name
 
-    @classmethod
-    def search_neighborhood_by_name(cls, search_term):
-        neighborhoods = cls.objects.filter(name__icontains=search_term)
-        return neighborhoods
-
-    @classmethod
-    def one_neighborhood(cls, id):
-        neighborhood = Neighborhood.objects.filter(id=id)
-        return neighborhood
-
-    @classmethod
-    def all_neighbourhoods(cls):
-        neighborhoods = cls.objects.all()
-        return neighborhoods
-
-    @classmethod
-    def get_neighborhood_by_id(cls, id):
-        neighborhood = Neighborhood.objects.filter(id=Neighborhood.id)
-        return neighborhood
-
-    @classmethod
-    def get_all_profiles(cls):
-        profile = Profile.objects.all()
-        return profile
-
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Profile(models.Model):
-    profile_image = models.ImageField(upload_to='profile_pics/')
-    neighbourhood_name = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
-    neighbourhood_description = models.TextField()
-
-    def __str__(self):
-        return self.name
+    Profile_photo = models.ImageField(upload_to = 'images/',blank=True)
+    Bio = models.TextField(max_length = 50,null = True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    neighborhood = models.ForeignKey('Neighborhood', related_name='neighbourhood',null=True)
+    email_address = models.CharField(max_length = 50,null = True)
+    business = models.ForeignKey('Business',related_name='business',null=True)
 
     def save_profile(self):
         self.save()
 
     @classmethod
-    def get_profile(cls, id):
-        profile = Profile.objects.get(user=id)
-        return profile
-
-    @classmethod
-    def get_all_profiles(cls):
-        profile = Profile.objects.all()
-        return profile
-
-    @classmethod
-    def find_profile(cls, search_term):
-        profile = Profile.objects.filter(user__username__icontains=search_term)
-        return profile
+    def get_by_id(cls, id):
+        details = Profile.objects.get(user = id)
+        return details
 
     @classmethod
     def filter_by_id(cls, id):
-        profile = Profile.objects.filter(user=id).first()
-        return profile
+        details = Profile.objects.filter(user = id).first()
+        return details
+    
+    @classmethod
+    def search_user(cls, name):
+        userprof = Profile.objects.filter(user__username__icontains = name)
+        return userprof
 
-    class Meta:
-        ordering = ['user']
+class NeighborHood(models.Model):
+    view = models.ImageField(upload_to = 'images/',blank=True)
+    name = models.CharField(max_length=50,blank=True)
+    location = models.CharField(max_length = 50,null = True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    occupants = models.ForeignKey(User, null = True,related_name='business')
+    
+    def save_hood(self):
+        self.save()
+
+    @classmethod
+    def get_all_hoods(cls):
+        hood = NeighborHood.objects.all()
+        return hood
 
 
 class Business(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(blank=True, max_length=30)
-    email = models.EmailField(max_length=70, blank=True)
-    business_owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    business_hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='biz', null=True)
+    name = models.CharField(max_length=50,blank=True)
+    image = models.ImageField(upload_to = 'images/')
+    user = models.ForeignKey(User, null = True,related_name='user')
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    neighborHood = models.ForeignKey(NeighborHood, null = True,related_name='business')
+    class Meta:
+        ordering = ['-pk']
+
+    def save_business(self):
+        self.save()
 
     @classmethod
-    def search_by_name(cls, search_term):
-        businesses = cls.objects.filter(name__icontains=search_term)
-        return businesses
+    def get_business(cls, profile):
+        business = Business.objects.filter(Profile__pk = profile)
+        return business
+    
+    @classmethod
+    def get_all_business(cls):
+        project = Project.objects.all()
+        return project
 
     @classmethod
-    def get_neighborhood_businesses(cls, neighborhood_id):
-        businesses = Business.objects.filter(neighborhood_id=id)
-        return businesses
-
-    @classmethod
-    def get_hood_business(cls, business_hood):
-        businesses = Business.objects.filter(business_hood_pk=business_hood)
-        return businesses
-
-    @classmethod
-    def get_profile_businesses(cls, profile):
-        businesses = Business.objects.filter(business_owner__pk=profile)
-        return businesses
-
-class Join(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    hood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user_id
+    def search(cls,search_term):
+        biz = cls.objects.filter(name__icontains=search_term)
+        return biz
 
 class Post(models.Model):
+    name = models.CharField(max_length=50,blank=True)
+    image = models.ImageField(upload_to = 'images/')
+    description = models.TextField(max_length = 50,null = True)
+    user = models.ForeignKey(User, null = True,related_name='post')
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    neighborHood = models.ForeignKey(NeighborHood, null = True,related_name='posts')
+    
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def get_hood_posts(cls,id):
+        posts = Post.objects.filter(id = id)
+        return posts
+
+class Comment(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='images/', blank=True)
-    description = models.TextField(blank=True)
-    poster = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    post_hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField(auto_now_add=True)
-    objects = models.Manager()
+    post = models.ForeignKey(Post,null = True)
+    neighborhood = models.ForeignKey(NeighborHood,related_name='comment')
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
 
     @classmethod
-    def search_post(cls, search_term):
-        posts = cls.objects.filter(name__icontains=search_term)
-        return posts
-
-    @classmethod
-    def get_hood_posts(cls, post_hood):
-        posts = Post.objects.filter(post_hood=id)
-        return posts
-
-    @classmethod
-    def search_by_name(cls, search_term):
-        posts = cls.objects.filter(name__icontains=search_term)
-        return posts
-
-    @classmethod
-    def all_posts(cls,id):
-        posts = Post.objects.all()
-        return posts
+    def find_commentpost(cls,id):
+        comments = Comments.objects.filter(post__pk = id)
+        return comments
