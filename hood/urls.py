cdfde4ac2274f46fe6c893215cfb45412a712eca
Nkurumwa <nkurumwa51@gmail.com>
2019-12-02 15:20:24 +0300

    adds app urls

--- hood/urls.py
+++ hood/urls.py
@@ -1,21 +1,19 @@
 from django.conf.urls import url
-from .import views
-from django.conf import settings
+from . import views
 from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [
-    url('^$',views.index,name='index'),
-    url(r'^create/profile/$',views.create_profile,name='create-profile'),
-    url(r'^home/$',views.home,name='home'),
-    url(r'^add/hood/$',views.add_hood,name='add_hood'),
-    url(r'^join_hood/<int:hood_id>$',views.join_hood,name='join_hood'),
-    url(r'^leave_hood/<int:hood_id>$',views.leave_hood,name='leave_hood'),
-    url(r'^add/business/$',views.add_business,name='add_business'),
-    url(r'^add/post/$',views.add_post, name='add_post'),
-    url(r'^user/<username>$',views.user_profile,name='user_profile'),
-    url(r'^search_results/$',views.search_results,name='search_results'),
-
+urlpatterns=[
+    url('^$',views.home_page,name = 'home_page'),
+    url(r'^edit$', views.edit, name='edit_profile'),
+    url(r'^upload/$', views.upload_business, name='upload_business'),
+    url(r'^hood/$', views.add_hood, name='add_hood'),
+    url(r'^join(?P<neighborhood_id>\d+)',views.join, name='join'),
+    url(r'^leave/(?P<neighborhood_id>\d+)',views.leave, name='leave'),
+    url(r'^one_hood(?P<neighborhood_id>\d+)',views.hood, name='hood'),
+    url(r'^comment/(?P<post_id>\d+)', views.one_post, name='comment'),
+    url(r'^post/$', views.add_post,name='add_post'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
