1dba4cefcc477c81273aaa1e10aa53fdffc7ab64
Nkurumwa <nkurumwa51@gmail.com>
2019-12-02 11:32:17 +0300

    adds base file content

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'hood.apps.HoodConfig',
     'hood',
     'bootstrap3',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
